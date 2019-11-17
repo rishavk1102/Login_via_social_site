3e88126ad7f85597938d0d4ef15d4d66a0bf45b2
Rishav <rishavk1102@gmail.com>
2019-11-18 02:15:44 +0530

    Initial commit : login with Facebook have a look at : https://developers.facebook.com/docs/facebook-login/android/
    Add hashkey
    Have a look at strings.xml and AndroidManifest.xml

---
+++ app/src/main/java/com/rishav/loginviasocialsite/MainActivity.java
@@ -0,0 +1,59 @@
+package com.rishav.loginviasocialsite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_main);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" + "Auth Token: " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode , resultCode , data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
